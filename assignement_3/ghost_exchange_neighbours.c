55ae9888759855d16b870f9add590d1667bc195c
Anezka <anezkapotesilova@gmail.com>
2026-01-23 13:26:57 +0100

    Testing exchange process

--- assignement_3/ghost_exchange_neighbours.c
+++ assignement_3/ghost_exchange_neighbours.c
@@ -12,20 +12,120 @@
 int *get_nbours (int pid, int m, int n)
 {
   int *neighbours = (int *)malloc(4 * sizeof(int));
-  for (int i = 0; i < 4; i++) {
-    neighbours[i] = -1;
-  }
-
-  neighbours[0] = pid % n == 0 ? -1 : pid - 1;
-  neighbours[1] = pid % n == n - 1 ? -1 : pid + 1;
-  neighbours[2] = pid < n ? -1 : pid - n;
-  neighbours[3] = pid >= (m - 1) * n ? -1 : pid + n;
+  
+  // Left, right, top, bottom
+  neighbours[0] = (pid % n == 0) ? -1 : pid - 1;
+  neighbours[1] = (pid % n == n - 1) ? -1 : pid + 1;
+  neighbours[2] = (pid < n) ? -1 : pid - n;
+  neighbours[3] = (pid >= (m - 1) * n) ? -1 : pid + n;
 
   return neighbours;
 }
 
 
+void exchange_ghost_cells(int matrix, int num_rows, int num_cols, int *neighbours)
+{
+  // Create subarray type for column (excluding first and last rows)
+  int array_of_sizes[2] = {num_rows, num_cols};
+  int array_of_subsizes[2] = {num_rows - 2, 1};
+  int array_of_starts[2];
+  MPI_Datatype column_type;
+  MPI_Status status;
 
+  // 1. Send left and right columns
+  if (neighbours[0] != -1) {
+    // Send second column (column 1) to left neighbour
+    array_of_starts[0] = 1;
+    array_of_starts[1] = 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Send(matrix, 1, column_type, neighbours[0], 0, MPI_COMM_WORLD);
+    MPI_Type_free(&column_type);
+  }
+
+  if (neighbours[1] != -1) {
+    // Send second-to-last column (column num_cols-2) to right neighbour
+    array_of_starts[0] = 1;
+    array_of_starts[1] = num_cols - 2;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Send(matrix, 1, column_type, neighbours[1], 0, MPI_COMM_WORLD);
+    MPI_Type_free(&column_type);
+  }
+
+  // 2. Receive left and right ghost columns
+  if (neighbours[0] != -1) {
+    // Receive from left neighbour into column 0 (ghost column)
+    array_of_starts[0] = 1;
+    array_of_starts[1] = 0;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Recv(matrix, 1, column_type, neighbours[0], 0, MPI_COMM_WORLD, &status);
+    MPI_Type_free(&column_type);
+  }
+
+  if (neighbours[1] != -1) {
+    // Receive from right neighbour into column num_cols-1 (ghost column)
+    array_of_starts[0] = 1;
+    array_of_starts[1] = num_cols - 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Recv(matrix, 1, column_type, neighbours[1], 0, MPI_COMM_WORLD, &status);
+    MPI_Type_free(&column_type);
+  }
+
+  // 3. Send and receive top and bottom rows
+  array_of_subsizes[0] = 1;
+  array_of_subsizes[1] = num_cols - 2;
+
+  if (neighbours[2] != -1) {
+    // Send first data row (row 1) to top neighbour
+    array_of_starts[0] = 1;
+    array_of_starts[1] = 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Send(matrix, 1, column_type, neighbours[2], 0, MPI_COMM_WORLD);
+    MPI_Type_free(&column_type);
+  }
+
+  if (neighbours[3] != -1) {
+    // Send last data row (row num_rows-2) to bottom neighbour
+    array_of_starts[0] = num_rows - 2;
+    array_of_starts[1] = 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Send(matrix, 1, column_type, neighbours[3], 0, MPI_COMM_WORLD);
+    MPI_Type_free(&column_type);
+  }
+
+  if (neighbours[2] != -1) {
+    // Receive from top neighbour into row 0 (ghost row)
+    array_of_starts[0] = 0;
+    array_of_starts[1] = 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Recv(matrix, 1, column_type, neighbours[2], 0, MPI_COMM_WORLD, &status);
+    MPI_Type_free(&column_type);
+  }
+
+  if (neighbours[3] != -1) {
+    // Receive from bottom neighbour into row num_rows-1 (ghost row)
+    array_of_starts[0] = num_rows - 1;
+    array_of_starts[1] = 1;
+    MPI_Type_create_subarray(2, array_of_sizes, array_of_subsizes, 
+                             array_of_starts, MPI_C_ORDER, MPI_INT, &column_type);
+    MPI_Type_commit(&column_type);
+    MPI_Recv(matrix, 1, column_type, neighbours[3], 0, MPI_COMM_WORLD, &status);
+    MPI_Type_free(&column_type);
+  }
+} 
 
 
 // int main (int argc, char *argv[])
